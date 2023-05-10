9a87aa1f9b724aeecee4ef421b765abd7c107c0b
tejalCHS <tejalg@canadahelps.org>
2023-05-10 13:00:43 -0400

    CRM-1821: minor change

--- CRM/Cdntaxreceipts/Form/ViewTaxReceipt.php
+++ CRM/Cdntaxreceipts/Form/ViewTaxReceipt.php
@@ -48,7 +48,7 @@ class CRM_Cdntaxreceipts_Form_ViewTaxReceipt extends CRM_Core_Form {
       $existingReceipt = cdntaxreceipts_load_receipt($receiptId);
       //CRM-1821 Show replaced receipt info on the Tax Receipt details page after receipt being replaced successfully
       if ($existingReceipt['receipt_status'] == 'issued') {
-        list($receipt_number, $receipt_id) = cdntaxreceipts_receipt_number($contributionId,TRUE);
+        list($receipt_number, $receipt_id) = CRM_Canadahelps_TaxReceipts_Receipt::receiptNumber($contributionId,TRUE);
         if(isset($receipt_number))
         $existingReceipt['cancelled_replace_receipt'] = $receipt_number;
       }
