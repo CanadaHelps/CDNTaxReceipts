4555f6cdfd3e7f53c142007825dd9f53064e5a0b
Celine Nicolas <celinen@canadahelps.org>
2023-06-16 17:39:18 -0400

    CRM-1820 allowing to replace receipts for Issue Separate Tax Receipts function (not fully working)

--- CRM/Cdntaxreceipts/Task/IssueSingleTaxReceipts.php
+++ CRM/Cdntaxreceipts/Task/IssueSingleTaxReceipts.php
@@ -213,6 +213,14 @@ class CRM_Cdntaxreceipts_Task_IssueSingleTaxReceipts extends CRM_Contribute_Form
       if ( cdntaxreceipts_eligibleForReceipt($contribution->id) ) {
 
         list($issued_on, $receipt_id) = cdntaxreceipts_issued_on($contribution->id);
+
+        // check if most recent is cancelled, and mark as "replace"
+        $cancelledReceipt = CRM_Canadahelps_TaxReceipts_Receipt::receiptNumber($contribution->id, true);
+        if ($cancelledReceipt[0] != NULL && $receipt_id == $cancelledReceipt[1]) {
+          $contribution->cancelled_replace_receipt_number  = $cancelledReceipt[0];
+          $contribution->replace_receipt  = 1;
+          $issued_on = '';
+        }
         if ( empty($issued_on) || ! $originalOnly ) {
           //CRM-918: Thank-you Email Tool
           if($this->getElement('thankyou_email')->getValue()) {
