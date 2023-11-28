2da2b8eeb881a3cf3443d38274e0e0a0c0d00315
CH-Rajal <rajalg@canadahelps.com>
2023-11-28 13:39:51 -0500

    CRM-2135-USD is not getting converted while issuing tax receipts

--- CRM/Cdntaxreceipts/Task/IssueSingleTaxReceipts.php
+++ CRM/Cdntaxreceipts/Task/IssueSingleTaxReceipts.php
@@ -410,6 +410,10 @@ class CRM_Cdntaxreceipts_Task_IssueSingleTaxReceipts extends CRM_Contribute_Form
               case 'Receipted Outside of DMS':
                 $result['eligibility_fix'] = "Receipted Outside of DMS.";
                 break;
+
+              case 'Currency Not Supported':
+                $result['eligibility_fix'] = "Only ".CRM_Core_Config::singleton()->defaultCurrency." amounts can be receipted. Please convert currency to ".CRM_Core_Config::singleton()->defaultCurrency." to issue tax receipt.";
+                break;
             }
           }
         }
